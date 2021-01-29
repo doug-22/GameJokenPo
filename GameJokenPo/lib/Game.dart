75c84a9bc132bd4dac79d5fb175ca3b0ffabb8b9
Douglas Oliveira <douglas.aguiar410@gmail.com>
2021-01-29 12:44:38 -0300

    💄 chore: Creating game interface

---
+++ GameJokenPo/lib/Game.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class Game extends StatefulWidget {
+  @override
+  _GameState createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("JokenPo"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              "Escolha do App",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Image.asset("imagens/padrao.png"),
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              "Escolha uma opção abaixo",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Image.asset("imagens/pedra.png", height: 100,),
+              Image.asset("imagens/papel.png", height: 100,),
+              Image.asset("imagens/tesoura.png", height: 100,)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
